e2a33e33ff1d804bb2565de5dd86fd55aeb4c7b1
saboobaker <saboobaker@gmail.com>
2020-07-16 09:55:20 -0500

    update

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -50,7 +50,8 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.info = patient.text.div 
+          p.info = patient.text.div;
+		  console.log(patient.text.div)
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }

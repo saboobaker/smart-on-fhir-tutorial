22df443705509ea2449aaba78a5f4d4ebe39ec42
Sohail-Aboobaker <sohail.aboobaker@va.gov>
2020-08-12 13:18:43 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,7 +15,7 @@
 		  console.log(smart.user);
 		  console.log(smart.userId);
 		  var user = smart.user;
-		  var us = user.read();
+		  var us = smart.get(smart.userId);//user.read();
           console.log(us);		  
 	  $.when(us).fail (onError);  
 	  $.when(us).done(function (user) {

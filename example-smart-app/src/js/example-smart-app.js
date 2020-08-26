c0a27e247ee6996485e27ebf67c0c8ce5623a631
Sohail-Aboobaker <sohail.aboobaker@va.gov>
2020-08-26 14:34:06 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -98,6 +98,9 @@ function onError() {
 		  subject: {
 			  reference: `Patient/${data.id}`
 		  },
+		  type: { 
+		    coding: {[{system:"some system", code: "30202"}]}			  
+		  },
 		  attachment: {
 		     contentType: 'application/xhtml+xml;charset=utf-8',
 		     content:'PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XM0MvL0RURCBYSFRNTCAxLjEvL0VOIg0KImh0dHA6Ly93d3cudzMub3JnL1RSL3hodG1sMTEvRFREL3hodG1sMTEuZHRkIj4NCjxodG1sIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hodG1sIj4NCjxoZWFkPg0KICA8dGl0bGU+U0lDIERvY3VtZW50PC90aXRsZT4NCjwvaGVhZD4NCjxib2R5Pg0KVGhpcyBpcyBhIHRlc3RpbmcNCjwvYm9keT4NCjwvaHRtbD4='
@@ -106,6 +109,11 @@ function onError() {
 	  }
 
 /*
+{"resourceType":"DocumentReference",
+"subject":{
+	"reference":"Patient/12724065"
+	},"attachment":{"contentType":"application/xhtml+xml;charset=utf-8","content":"PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XM0MvL0RURCBYSFRNTCAxLjEvL0VOIg0KImh0dHA6Ly93d3cudzMub3JnL1RSL3hodG1sMTEvRFREL3hodG1sMTEuZHRkIj4NCjxodG1sIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hodG1sIj4NCjxoZWFkPg0KICA8dGl0bGU+U0lDIERvY3VtZW50PC90aXRsZT4NCjwvaGVhZD4NCjxib2R5Pg0KVGhpcyBpcyBhIHRlc3RpbmcNCjwvYm9keT4NCjwvaHRtbD4="}
+}
 {
   "resourceType": "DocumentReference",
   "subject": {

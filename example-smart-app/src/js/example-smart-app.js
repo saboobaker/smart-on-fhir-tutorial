263996e703fc36493abd8379b98531bf52554c9b
Sohail-Aboobaker <sohail.aboobaker@va.gov>
2020-08-26 15:49:59 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -105,7 +105,7 @@ function onError() {
 		     contentType: 'application/xhtml+xml;charset=utf-8',
 		     content:'PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XM0MvL0RURCBYSFRNTCAxLjEvL0VOIg0KImh0dHA6Ly93d3cudzMub3JnL1RSL3hodG1sMTEvRFREL3hodG1sMTEuZHRkIj4NCjxodG1sIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hodG1sIj4NCjxoZWFkPg0KICA8dGl0bGU+U0lDIERvY3VtZW50PC90aXRsZT4NCjwvaGVhZD4NCjxib2R5Pg0KVGhpcyBpcyBhIHRlc3RpbmcNCjwvYm9keT4NCjwvaHRtbD4='
 		     },
-         indexed: '2015-11-18T18:00:00Z',
+         indexed: new Date(),
           status: 'current',			 
          docStatus: {
                  coding: [

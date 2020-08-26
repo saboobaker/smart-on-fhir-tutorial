22450e5ceb49ee27f83f906b57a1435e8ad6c16c
Sohail-Aboobaker <sohail.aboobaker@va.gov>
2020-08-26 14:10:14 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -90,7 +90,7 @@ function onError() {
    // FHIR.oauth2.ready().then(onReady).catch(onError);;
     return ret.promise();
 
-  };
+  
 
   function getDocument(data) {
 	  return{
@@ -103,7 +103,7 @@ function onError() {
 		     content:'PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XM0MvL0RURCBYSFRNTCAxLjEvL0VOIg0KImh0dHA6Ly93d3cudzMub3JnL1RSL3hodG1sMTEvRFREL3hodG1sMTEuZHRkIj4NCjxodG1sIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hodG1sIj4NCjxoZWFkPg0KICA8dGl0bGU+U0lDIERvY3VtZW50PC90aXRsZT4NCjwvaGVhZD4NCjxib2R5Pg0KVGhpcyBpcyBhIHRlc3RpbmcNCjwvYm9keT4NCjwvaHRtbD4='
 		     }]
 		  }
-	  };
+	  }
 /*
 {
   "resourceType": "DocumentReference",
